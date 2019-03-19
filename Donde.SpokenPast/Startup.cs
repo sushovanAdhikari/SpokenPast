f03f61d5b4be063fd7da4ff20ada2af62d3a7756
Reid <reidmmarkham@gmail.com>
2019-03-19 06:19:01 -0500

    Cleaning up previous commit

--- Donde.SpokenPast/Startup.cs
+++ Donde.SpokenPast/Startup.cs
@@ -3,7 +3,6 @@ using Donde.SpokenPast.Core.Repositories.Interfaces.RepositoryInterfaces;
 using Donde.SpokenPast.Core.Service.Interfaces.ServiceInterfaces;
 using Donde.SpokenPast.Core.Services.Services;
 using Donde.SpokenPast.Infrastructure.Repositories;
-using Donde.SpokenPast.Infrastructure.Repositories;
 using Donde.SpokenPast.Web.OData;
 using Microsoft.AspNet.OData.Builder;
 using Microsoft.AspNetCore.Builder;
@@ -13,7 +12,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,10 +24,9 @@ namespace Donde.SpokenPast.Web
 {
     public class Startup
     {
-
         public Startup(IHostingEnvironment env, IConfiguration config)
         {
-            Configuration = config as IConfigurationRoot;        
+            Configuration = config as IConfigurationRoot;
             CurrentEnvironment = env;
         }
 
@@ -53,14 +50,6 @@ namespace Donde.SpokenPast.Web
             services.AddDondeOData(Configuration);
 
             services.AddMvc();
-
-            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
-
-            //container.Register<IAugmentObjectService, AugmentObjectService>(Lifestyle.Scoped);
-
-            services.AddSingleton<IControllerActivator>(new SimpleInjectorControllerActivator(container));
-
-            services.UseSimpleInjectorAspNetRequestScoping(container);
 
         }
 
@@ -95,26 +84,17 @@ namespace Donde.SpokenPast.Web
                 routes.MapRoute("default", "{controller=Home}/{action=Index}");
             });
             InitializeAndVerifyContainer(app, loggerFactory);
-
         }
-        private static void InitializeContainer(Container container)
-        {
-            container.Register<AugmentObjectService>();
 
-        private void InitializeAndVerifyContainer(IApplicationBuilder app, ILoggerFactory loggerFactory)
-
-            // Trying to include Identity into Simple Injector - please ignore
-            // container.Register<IUserStore<ApplicationUser>>(() => new UserStore<ApplicationUser>(new Donde.SpokenPast.Infrastructure.Database.DondeContext()));
-        }
         private void InitializeAndVerifyContainer(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
             var connectionString = Configuration["Donde.SpokenPast.Data:API:ConnectionString"];
             DondeSpokenPastBootstrapper.BootstrapDondeSpokenPast
-                (container, 
+                (container,
                 Assembly.GetExecutingAssembly(),
-                connectionString, 
+                connectionString,
                 CurrentEnvironment.EnvironmentName, loggerFactory);
-          
+
             // Allow Simple Injector to resolve services from ASP.NET Core.
             container.AutoCrossWireAspNetComponents(app);
 
